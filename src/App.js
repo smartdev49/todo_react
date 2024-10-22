c5d6a823a0f1016139350e623fcf0782f5c4d85d
unknown <curtisjowen@gmail.com>
2024-10-22 02:41:41 -0400

    count todo list

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      userName: "Adam"
+      userName: "Adam",todoItems: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false},
+        { action: "Collect Tickets", done: true},
+        { action: "Call Joe", done: false},
+      ],
+      newItemText: ""
     }
   }
 
@@ -14,6 +20,22 @@ export default class App extends Component {
     this.setState({
       userName: this.state.userName === "Adam" ? "Bob" : "Adam"
     })
+  }
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  }
+  
+  createNewTodo = () => {
+    console.log(this.state.todoItems);
+    if (!this.state.todoItems.find(item=> item.action === this.state.newItemText)) {
+      this.setState({
+        todoItems: [...this.state.todoItems,
+          {action: this.state.newItemText, done:false}
+        ],
+        newItemText: ""
+      })
+    }
   }
 
   render() {
@@ -24,9 +46,20 @@ export default class App extends Component {
             To Do List
           </h4>
         </div>
+        <div className="container-fluid">
+          <div className="my-1">
+            <input className="form-control" 
+              value={ this.state.newItemText } 
+              onChange={ this.updateNewTextValue } />
+            <button className="btn btn-primary mt-1"
+              onClick={ this.createNewTodo }
+              value={ this.state.newItemText }>Add</button>
+          </div>
+        </div>
         <div>
           <h4 className="bg-primary text-white text-center p-2">
             { this.state.userName }'s To Do List
+            ({ this.state.todoItems.filter(t => !t.done).length} items to do)
           </h4>
         </div>
         <button className="btn btn-primary m-2" onClick={ this.changeStateData }>
